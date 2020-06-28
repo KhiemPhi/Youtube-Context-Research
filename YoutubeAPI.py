62db5f1f976617d07193af0214af703fed57db18
Khiem Phi <khiemais@gmail.com>
2020-06-28 11:48:17 -0400

    Adding New .json Data to Repo, Fix Bugs for Searching Videos

--- YoutubeAPI.py
+++ YoutubeAPI.py
@@ -1,228 +1,276 @@
 from apiclient.discovery import build
 from datetime import datetime
 import io
-import pickle
-
 from googleapiclient.http import MediaIoBaseDownload
 from youtube_transcript_api import YouTubeTranscriptApi
+import argparse
+import json
 
-api_key = ""
-youtube = build("youtube" , "v3" , developerKey=api_key)
-channel_id = "UCupvZG-5ko_eiXAupbDfxWw" #CNN
+
+# Retrieving Channel Ids
+api_key = "AIzaSyCsCfOEng2WhtMHQoCi0FX48OPB5tw-F5g"
+youtube = build("youtube", "v3", developerKey=api_key)
+
 
 def print_title_from_search_results_video(res):
 	for item in res['items']:
 		print(item['snippet']['title'])
 
+
 def print_channel_data(res):
 	for item in res['items']:
 		print(item['snippet'])
 
+
 def search_videos(query):
-	req = youtube.search().list(q=query, part='snippet' , type='video' , maxResults=5) # Max 5 results, highest is 50 results, Searching for videos
-	res = req.execute() #JSON Format
+	req = youtube.search().list(q=query, part='snippet', type='video',
+								maxResults=5)  # Max 5 results, highest is 50 results, Searching for videos
+	res = req.execute()  # JSON Format
 	return res
 
-def search_channels(query):
-	req = youtube.search().list(q=query, part='snippet' , type='channel' , maxResults=5) # Max 5 results, highest is 50 results, Searching for videos
-	res = req.execute() #JSON Format
-	return res
 
 def search_videos_by_dates(query, start_date, end_date):
-	req = youtube.search().list(q=query, part='snippet' , type='video' , publishedAfter=start_date , publishedBefore=end_date, maxResults=5) # Max 5 results, highest is 50 results, Searching for videos
-	res = req.execute() #JSON Format
+	req = youtube.search().list(q=query, part='snippet', type='video', publishedAfter=start_date,
+								publishedBefore=end_date,
+								maxResults=5)  # Max 5 results, highest is 50 results, Searching for videos
+	res = req.execute()  # JSON Format
 	return res
 
+
 def fetch_all_videos_by_channel(channel_id):
-	res = youtube.channels().list(id=channel_id , part='contentDetails').execute()
-	uploads_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads'] # Get uploaded playlist ids
-	 
+	res = youtube.channels().list(id=channel_id, part='contentDetails').execute()
+	uploads_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']  # Get uploaded playlist ids
+
 	videos = []
 	next_page_token = None
-	pages = 6 # Two Pages
+	pages = 6  # 6 Pages
 	i = 0
 
 	while i < pages:
-		res = youtube.playlistItems().list(playlistId = uploads_id, part="snippet" , maxResults=5 , pageToken = next_page_token).execute()
+		res = youtube.playlistItems().list(playlistId=uploads_id, part="snippet", maxResults=5,
+										   pageToken=next_page_token).execute()
 		videos += res['items']
 		next_page_token = res['nextPageToken']
 
 		if next_page_token is None:
 			break
 
-		i += 1	
+		i += 1
 
-	return videos # return list of videos
+	return videos  # return list of videos
+
 
 def print_title_from_videos_json(video):
 	print(video['snippet']['title'])
 
-def get_comment(video_id , next_page_token):
+
+def get_comment(video_id, next_page_token):
 	try:
-		if(next_page_token is not None):
-			comment_results = youtube.commentThreads().list(videoId=video_id , part="snippet" , maxResults=50, nextPageToken=next_page_token).execute()
+		if next_page_token is not None:
+			comment_results = youtube.commentThreads().list(videoId=video_id, part="snippet", maxResults=50,
+															pageToken=next_page_token).execute()
 		else:
-			comment_results = youtube.commentThreads().list(videoId=video_id , part="snippet" , maxResults=50).execute()
+			comment_results = youtube.commentThreads().list(videoId=video_id, part="snippet", maxResults=50).execute()
 		return comment_results
 	except:
 		return None
 
 
-def get_comments_by_video_id(video_id):	
+def get_comments_by_video_id(video_id):
 	comments = []
 	next_page_token = None
-	pages = 2 # Two Pages
+	pages = 2  # Two Pages
 	i = 0
 
 	while i < pages:
 		comment_results = get_comment(video_id, next_page_token)
-		if (comment_results is not None):			
-			comments.extend([item['snippet']['topLevelComment']['snippet']['textOriginal'] for item in comment_results['items']])
+		if comment_results is not None:
+			comments.extend(
+				[item['snippet']['topLevelComment']['snippet']['textOriginal'] for item in comment_results['items']])
 
 			next_page_token = comment_results.get('nextPageToken')
-
+			
 		else:
 			break
 
-		i+= 1
+		i += 1
 
-	return comments 
+	return comments
 
-def get_channel_id(channel_res):
-	return channel_res['snippet']['channelId']
 
 def get_video_id(video_res):
 	return video_res['snippet']['resourceId']['videoId']
 
-def filter_based_on_keywords(title, keywords):
-	return any(word in title for word in keywords)
-
 def get_transcripts(video_id):
 	try:
 		transcript = YouTubeTranscriptApi.get_transcript(video_id)
-		return transcript
 	except:
-		return None
+		return {}
+
+	return transcript
+
 
 def get_transcript_text(transcript):
-	return [x['text'] for x in transcript]
+	if transcript is not None:
+		return [x['text'] for x in transcript]
+	else:
+		return ""
 
+
+def construct_url(id):
+	base_url = "https://www.youtube.com/watch?v="
+	return base_url + id
+
+def filter_based_on_keywords(title, keywords):
+	return any(word in title.lower() for word in keywords) # Added lower case to improve searching
+
+def fetch_all_videos_by_channel_with_search(channel_id, keywords):
+	res = youtube.channels().list(id=channel_id, part='contentDetails').execute()
+	uploads_id = res['items'][0]['contentDetails']['relatedPlaylists']['uploads']  # Get uploaded playlist ids
+
+	videos = []
+	next_page_token = None
+	pages = 20  # 6 Pages
+	i = 0
+
+	while i < pages:	  		
+		res = youtube.playlistItems().list(playlistId=uploads_id, part="snippet", maxResults=50,
+										   pageToken=next_page_token).execute()
+
+		# Filter by keywords here 		
+		videos_to_add = list(filter(lambda video: filter_based_on_keywords(video['snippet']['title'], keywords), res['items']))
+		videos += videos_to_add
+
+		next_page_token = res['nextPageToken']
+
+		if next_page_token is None:
+			break
+
+		i += 1
+
+
+	return videos  # return list of videos	
+
+def get_channel_id(youtube, channel):
+	res_channels = youtube.search().list(q=channel, part='snippet', type='channel',
+								maxResults=5).execute()	
+	channel_id = res_channels['items'][0]['snippet']['channelId']
+	return channel_id
+
+def get_user_keywords(path, file_name):
+	user_keywords = open(path, "r+")
+	keywords_string = user_keywords.readlines()
+	keywords_string = [x.lower().rstrip() for x in keywords_string]
+	return keywords_string
+
+def write_to_json_one_video(title, URL, transcript,file_name):
+	data = {}
+	data['videos'] = []
+	data['videos'].append({
+		'title': title,
+		'URL' : URL,
+		'transcript': transcript,
+		'comments': comments
+	})
+
+	with open(file_name, 'w') as outfile:
+		json.dump(data, outfile)
+
+	return data	
+
+def write_to_json_batch(zip_data, file_name):
+	data = {}
+	data['videos'] = []
+	for video_data in zip_data:
+		data['videos'].append({
+			'title': video_data[0],
+			'URL' : video_data[1],
+			'transcript': video_data[2],
+			'comments': video_data[3]
+		})
+
+	with open(file_name, 'w') as outfile:
+		json.dump(data, outfile)
+
+	return data		
 
 def main():
+	# 10 Videos Per 2 Channels, 100 Comments / video , Transcripts For Each Video
+	# Fox News + CNN
+	# Transcript - Time stamp + language at time
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-c1", "--first_channel", required=False,
+	help="first channel to retrieve data")
+	parser.add_argument("-c2", "--second_channel", required=False,
+	help="second channel to retrieve data")	
+	parser.add_argument("-k", "--keywords_path", required=False, help = ".txt file containing keywords for searching")
 	
-	# Basic Demo To Make Simple Search Queries
-	#res_vids = search_videos('avengers')
-	#print_title_from_search_results_video(res_vids)	
+	args = vars(parser.parse_args())
+
+	# Getting User Input Args
+	keywords_path = str(args["keywords_path"]) if args["keywords_path"] else ""
+	channel_1 = str(args["first_channel"]) if args["first_channel"] else "fox news"
+	channel_2 = str(args["second_channel"]) if args["second_channel"] else "CNN"
+	default_keywords = ["riot", "george floyd", "protest", "crowd", "police", "black", "officer", "violent", "cop", "policing", "floyd"]
+	keywords =  get_user_keywords(keywords_path) if keywords_path is not "" else default_keywords
+
+
+	# Quick-start Test
+	#transcript_test = get_transcripts("Wf4cea5oObY")
+	#comments_test = ["Good", "Bad", "Okay"] #get_comments_by_video_id("Wf4cea5oObY")
+	#title = "Police: Last Week Tonight with John Oliver (HBO)"
+	#URL = construct_url("Wf4cea5oObY")
+	#write_to_json(title, URL, transcript_test, comments_test, "./CNN_Data/JohnOliver.json")
+
 	
-	'''
-	# Search by Restricted Series of Dates
-	start_date = datetime(year=2016, month=12 , day = 1).strftime('%Y-%m-%dT%H:%M:%SZ')
-	end_date = datetime(year=2017, month=2 , day = 1).strftime('%Y-%m-%dT%H:%M:%SZ')
-	res_travel_ban = search_videos_by_dates('trump travel ban', start_date , end_date)
-	print_title_from_search_results_video(res_travel_ban) # Print Video Titles Based On Topic, Can Increase Query or Make Channel Specific	
+	fox_news_channel_id = get_channel_id(youtube, channel_1)
+	cnn_channel_id = get_channel_id(youtube, channel_2)
 	
-	# Getting All The Videos Of A Channel
-	# Fetching from Upload Playlist : Get Details of Channel, Call Operation Over Playlist Item
-	res_CNN = fetch_all_videos_by_channel(channel_id) 
+	print("Searching For Videos ... ")
+	videos_fox_news = fetch_all_videos_by_channel_with_search(fox_news_channel_id, keywords)
+	videos_cnn = fetch_all_videos_by_channel_with_search(cnn_channel_id, keywords)	
+	print("Search Completed ... ")
 
-	# Getting the comments on a video
-	video_1_CNN_id = res_CNN[0]['snippet']['resourceId']['videoId'] 
-	video_1_CNN_title = res_CNN[0]['snippet']['title']
-	comments = get_comments_by_video_id(video_1_CNN_id)
+	# Fetch Title Of Searched Videos
+	title_fox_news = [video['snippet']['title'] for video in videos_fox_news]
+	title_cnn = [video['snippet']['title'] for video in videos_cnn] 
 
+	# Fetch ID of Searched Videos
+	video_id_fox_news = [get_video_id(video) for video in videos_fox_news]
+	video_id_cnn = [get_video_id(video) for video in videos_cnn]
 
-	# Getting the captions on a video  (Hillbilly HotDogs Example)
-	transcript = YouTubeTranscriptApi.get_transcript("KySLSYSTbcE")
-	transcript_text = [x['text'] for x in transcript]
-	print(transcript_text)
-	'''
+	# Construct URL of Searched Videos
+	video_url_fox_news = [construct_url(x) for x in video_id_fox_news]
+	video_url_cnn = [construct_url(x) for x in video_id_cnn]
 
-	# 10 Videos Per 2 Channels, 100 Comments / video , Transcripts For Each Video 
-	# Fox News + CNN 
-	# Transcript - Time stamp + language at time 
+	# Fetch Comments By ID (100 Comments Per Video)
+	comments_fox_news = [get_comments_by_video_id(video_id) for video_id in video_id_fox_news]
+	comments_cnn = [get_comments_by_video_id(video_id) for video_id in video_id_cnn]
 
-	res_channels_fox_news = search_channels('fox news')
-	res_channels_CNN = search_channels('CNN')
+	# Fetch Transcripts With Time Stamp
+	transcript_fox_news =  [get_transcripts(video_id) for video_id in video_id_fox_news]
+	transcript_cnn =  [get_transcripts(video_id) for video_id in video_id_cnn]
+
+	# Fetch Transcripts W/o Time Stamp
+	transcript_fox_news_no_time_stamp =  [get_transcript_text(transcript) for transcript in transcript_fox_news]
+	transcript_cnn_no_time_stamp =  [get_transcript_text(transcript) for transcript in transcript_cnn]
 	
-	fox_news_channel_id = get_channel_id(res_channels_fox_news['items'][0])
-	CNN_channel_id = get_channel_id(res_channels_CNN['items'][0])
+	# After this pre-processing phase, we have:
+	# Title 
+	# Video URL
+	# Video ID
+	# Transcript w/ Time Stamp	
+	# Comments
 
-	videos_fox_news = fetch_all_videos_by_channel(fox_news_channel_id)
-	videos_CNN = fetch_all_videos_by_channel(CNN_channel_id)
+	# Moving To JSON
+	data_zip_fox_news = list(zip(title_fox_news, video_url_fox_news, transcript_fox_news, comments_fox_news))
+	data_zip_cnn = list(zip(title_cnn, video_url_cnn, transcript_cnn, comments_cnn))
 
-	title_fox_news = [ video['snippet']['title'] for video in videos_fox_news]
-	title_CNN = [ video['snippet']['title'] for video in videos_CNN]
+	print("Writing To JSON Files .... ")
+	write_to_json_batch(data_zip_fox_news, "fox_news_data.json")
+	write_to_json_batch(data_zip_cnn, "cnn_data.json")
+	print("Finishing Writing To JSON Files .... ")
 
-	video_id_fox_news = [ get_video_id(video) for video in videos_fox_news]
-	video_id_CNN      = [ get_video_id(video) for video in videos_CNN]
-
-	title_to_id_dict_fox_news = dict(zip(title_fox_news , video_id_fox_news))
-	title_to_id_dict_CNN = dict(zip(title_CNN, video_id_CNN))
-
-
-	#Filter out based on keywords
-	# Keywords: riot , george floyd, riot, protest, crowd, police, black people, officer, police
-	keywords = ["riot", "george floyd", "protest", "crowd", "police", "black people", "officer", "police", "violent"]
-	keywords_dict_fox_news = dict(filter(lambda x: filter_based_on_keywords(x[0], keywords)    , title_to_id_dict_fox_news.items()))
-	keywords_dict_CNN = dict(filter(lambda x: filter_based_on_keywords(x[0], keywords)    , title_to_id_dict_CNN.items()))
-	
-	#With the proper keywords filter out -> go directly to transcript + comments
-	transcript_fox_news = list(map(lambda x: get_transcripts(x[1])  , keywords_dict_fox_news.items()))
-	transcript_CNN = list(map(lambda x: get_transcripts(x[1])  , keywords_dict_CNN.items()))
-
-	#Creating title to transcript dict
-	title_to_transcript_fox_news =  dict(zip(title_fox_news , transcript_fox_news)) 
-	title_to_transcript_CNN      =  dict(zip(title_CNN, transcript_CNN))
-
-	#Filter out None:
-	title_to_transcript_fox_news = dict(filter(lambda x: x[1] is not None ,  title_to_transcript_fox_news.items()))
-	title_to_transcript_CNN = dict(filter(lambda x: x[1] is not None ,  title_to_transcript_CNN.items()))
-
-	#Text For Banerjee Ease of Access
-	transcript_fox_news_text = list(map(lambda x: get_transcript_text(x[1])  ,   title_to_transcript_fox_news.items()))
-	transcript_CNN_text      = list(map(lambda x: get_transcript_text(x[1]) , title_to_transcript_CNN.items()))
-	title_to_transcript_fox_news_text = dict(zip(title_fox_news , transcript_fox_news_text))
-	title_to_transcript_CNN_text     = dict(zip(title_CNN , transcript_CNN_text))
-
-	#Pkl Pure Text
-	pkl_title_to_text_fox_news = open("title_to_text_fox_news.pkl","wb")
-	pickle.dump(title_to_transcript_fox_news_text , pkl_title_to_text_fox_news)
-	pkl_title_to_text_fox_news.close()
-
-	pkl_title_to_text_CNN = open("title_to_text_CNN.pkl","wb")
-	pickle.dump(title_to_transcript_CNN_text , pkl_title_to_text_CNN)
-	pkl_title_to_text_CNN.close()
-
-	#Pkl Text With Time Stamps
-	pkl_title_to_transcript_fox_news = open("title_to_transcript_fox_news.pkl","wb")
-	pickle.dump(title_to_transcript_fox_news , pkl_title_to_transcript_fox_news)
-	pkl_title_to_transcript_fox_news.close()
-
-	pkl_title_to_transcript_CNN = open("title_to_transcript_CNN.pkl","wb")
-	pickle.dump(title_to_transcript_CNN , pkl_title_to_transcript_CNN)
-	pkl_title_to_transcript_CNN.close()
-
-	#Getting The Comments
-	fox_news_comments =  [get_comments_by_video_id(video_id) for video_id in video_id_fox_news]
-	CNN_comments = [get_comments_by_video_id(video_id)  for video_id in video_id_CNN]
-
-	#Zip Into Dict
-	title_to_comments_fox_news = dict(zip(title_fox_news , fox_news_comments))
-	title_to_comments_CNN = dict(zip(title_CNN , CNN_comments))
-
-	#Pkl title_to_comment Dict
-	pkl_title_to_comments_fox_news = open("title_to_comments_fox_news.pkl" , "wb")
-	pickle.dump(title_to_comments_fox_news , pkl_title_to_comments_fox_news)
-	pkl_title_to_comments_fox_news.close()
-
-	pkl_title_to_comments_CNN = open("title_to_comments_CNN.pkl" , "wb")
-	pickle.dump(title_to_comments_CNN , pkl_title_to_comments_CNN)
-	pkl_title_to_comments_CNN.close()
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+	main()
